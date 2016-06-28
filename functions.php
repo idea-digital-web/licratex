7aa79fc467b02f0081a0c079e204cee5adf4bb8d
Alex Ballera <alexballera@gmail.com>
2016-06-28 18:11:54 -0400

    Agregado flexslides

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php 
+
+/*
+Agregar moneda (Bs.)
+*/
+add_filter( 'woocommerce_currencies', 'add_my_currency' );
+function add_my_currency( $currencies ) {
+     $currencies['ABC'] = __( 'Bolívares Débiles', 'woocommerce' );
+     return $currencies;
+}
+add_filter('woocommerce_currency_symbol', 'add_my_currency_symbol', 10, 2);
+function add_my_currency_symbol( $currency_symbol, $currency ) {
+     switch( $currency ) {
+          case 'ABC': $currency_symbol = 'Bs.'; break;
+     }
+     return $currency_symbol;
+}
+ ?>
